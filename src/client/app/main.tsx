655af115d9e6e587432fae8fba9a6fd73850a74c
Cepoglazik <cpo.gla3ik@gmail.com>
2026-05-13 10:45:19 +0300

    Vite config base name changed

--- src/client/app/main.tsx
+++ src/client/app/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Index from '@client/pages/Index/Index';
 import './main.scss'
-/* import Data from '@client/features/Data'; */
 
 /* createRoot(document.getElementById('root')!).render(
   <Index />
@@ -12,7 +11,4 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Index />
   </StrictMode>
-)
-
-/* new Data("name", {});
-new Data("name", {}); */
+);
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my_website/',
+  base: '/NoteFlow/',
   css: {
     preprocessorOptions: {
       scss: {
