71a19c8a36cfbd4ff3bb2e6ed33a6d565eba0a92
Cepogla3ik <tsurkan.sergij@gmail.com>
2026-05-20 23:56:44 +0300

    Fixed

--- src/client/pages/Index/Main/Board/Board.interface.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import type { BoardItemsProps } from "@shared/components/BoardItems/BoardItemsProps.interface";
-
-type BoardContent = React.ReactElement<BoardItemsProps>;
-
-export interface BoardProps {
-  content: BoardContent
-}
